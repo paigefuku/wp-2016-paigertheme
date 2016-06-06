fb7f31c2077602fc2fa388512f43076cdff12a33
Paige Fukuhara <paigefukuhara@gmail.com>
2016-06-05 19:05:40 -0700

    First Commit
    I think I’m doing this right? Adding files.

---
+++ wp-content/themes/paiger/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Theme Name: Paiger 2016
+Author: Paige Fukuhara
+Author URI: http://paigefukuhara.com
+Description: What I always imagined my own site to look like. Nothing super exciting. Clean, white, simple. 
+Version: 1.0
+*/
+
+?>
---
+++ wp-content/themes/paiger/index.php
@@ -0,0 +1,137 @@
+
+
+<!DOCTYPE html>
+<!--html template for Wordpress Main Page | WEB 170 | SPRING 2016-->
+<html>
+
+    <head>
+        <meta charset="utf-8">
+
+        <title>Paige Fukuhara | Creative Services, Art &amp; UX &amp; UI Consultant | Seattle WA | Photographer, Illustrator, Visual Strategist</title>
+     
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/responsiveslides.js"></script>  
+<script src="<?php bloginfo('template_directory'); ?>/js/responsiveslides.min.js"></script>      
+<style>
+    
+
+        </style>      
+    </head>
+    <body>
+        
+  <header><a href="home.html">   
+<div id="logoHeader"><!--BEGIN logoHeaderdiv-->
+       <img src="<?php bloginfo('template_directory'); ?>/images/papers.png" id="flyingpapers" alt="banner-top"> 
+        <div id="wordsHeader"><!--BEGIN wordsHeader div-->
+       <h1>Paige Fukuhara</h1>
+        <h2>Creative Services</h2>
+        </div>   <!--END wordsHeader div-->
+    </div><!--End logoHeaderdiv-->
+        </a>
+    <div id="menu"><!--BEGIN menu div-->
+        <ul id="navmenu">
+        <li>About
+            <ul class="subnavmenu">
+            <li class="subnavmenuLI"><a href="main.html">Bio</a></li>
+            <li class="subnavmenuLI"><a href="main.html">Process &amp; Methods</a></li>
+            <li class="subnavmenuLI"><a href="main.html">Friends &amp; Colleages</a></li>
+             <li class="subnavmenuLI"><a href="main.html">CV // RESUME</a></li>
+            </ul>
+            
+            </li>
+        <li>Services   
+         <ul class="subnavmenu">
+            <li class="subnavmenuLI"><a href="main.html">Web Services</a></li>
+            <li class="subnavmenuLI"><a href="#main.html">Illustration</a></li>
+            <li class="subnavmenuLI"><a href="main.html">Photography</a></li>
+             <li class="subnavmenuLI"><a href="main.html">Paper Crafts</a></li>
+            </ul>
+            </li> 
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Contact</a></li>
+        </ul>    
+    </div><!--END menu div-->
+    </header>
+ 
+<article class="intro">
+
+
+<div id="lasagna"><!--BEGIN lasagna div-->
+    <h2>Corn Amaranth Maize Bunya Creative Services Creative Services</h2>
+         <section>
+        <div id="caesarSalad"><!--BEGIN caesarSalad div-->
+        <h3>Endive Cauliflower Sea Lettuce Kohlrabi Amaranth </h3>
+       <blockquote>  
+         <p> Kohlrabi amaranth. Pumpkin onion chickpea a Corn amaranth salsify the gram corn! Swiss chard.</p>
+            </blockquote>
+        <p>Fennel kombu maize bamboo shoot <a href="#">green beans</a> swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander Water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean <a href="#">chickweed potato</a> bell pepper artichoke. </p> 
+        <p>Soko radicchio bunya nuts gram dulse silver beet parsnip napa cabbage lotus root sea lettuce brussels sprout cabbage. Catsear cauliflower garbanzo yarrow <a href="#">salsify chicory</a> garlic bell pepper napa cabbage lettuce tomato kale arugula melon sierra leone bologi rutabaga tigernut. Sea lettuce gumbo grape <a href="#">kale kombu cauliflower</a>.
+    </p>
+       
+     <p>Pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Nuts nori azuki bean chickweed potato bell pepper artichoke. </p> 
+            
+     <h3>Nuts nori azuki bean chickweed </h3>       
+        <p>Endive cauliflower sea lettuce kohlrabi amaranth water. Spinach avocado daikon napa cabbage asparagus winter. Purslane kale. Celery potato scallion desert raisin horseradish</p>
+        <ol>
+            <li>Collard greens</li>
+            <li>Radicchio</li>
+            <li>Kale </li>
+            <li>Grape kombu </li>
+            <li>Garlic gram </li>
+            <li>Potato bell pepper</li>
+        </ol>
+        <p>Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.</p>
+<figure><img src="<?php bloginfo('template_directory'); ?>/imagesimages/plants_48.jpg" alt="flowers"><figcaption><p>Carrot watercress. 1998 maize bamboo shoot, lettuce tomato.</p></figcaption></figure>
+<p>Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts. Fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.</p>
+        </div><!--END caesarSalad div-->
+        
+   </section>
+     
+   </div><!--END lasagna div-->
+        
+
+    
+    <div id="broccoli"><!--BEGIN broccoli div-->
+        <section>
+        <h4>Corn amaranth salsify bunya nuts nori </h4>
+        
+            <p>Lotus root water spinach. Carrot watercress. Azuki bean chickweed potato bell pepper artichoke. </p>   
+        <ul>
+            <li>Bamboo shoot green bean </li>
+            <li>Spinach carrot</li>
+            <li>Beetroot</li>
+            <li>Coriander</li>
+
+        </ul>
+        
+                <h4>Melon Radish Asparagus </h4>
+        
+            <p>Lotus root water spinach. Carrot watercress. Azuki bean chickweed potato bell pepper artichoke. </p>   
+        <ul>
+            <li>Water chestnut </li>
+            <li><a href="#">Azuki</a></li>
+            <li>Chickpea</li>
+            <li><a href="#">Nuts nori</a></li>
+
+        </ul>
+    </section>
+    </div><!--END broccoli div-->
+    
+    
+        
+ </article>             
+        
+    <footer>
+        <div id="copyright">
+            <p>Site design, contents, all imagery &copy; Paige Fukuhara 2016</p>
+        </div>
+    </footer><!-- END FOOTER  -->
+        <script>
+  $(function() {
+    $(".rslides").responsiveSlides();
+  });
+</script>
+    </body>
+</html>
+
