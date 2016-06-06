0c22e83d42e24719926e815c08cf9a6d441039ec
Paige Fukuhara <paigefukuhara@gmail.com>
2016-06-05 19:11:19 -0700

    still getting used to this...
    added credentials

---
+++ credentials.php
@@ -0,0 +1,10 @@
+<?php
+
+//credentials.php for wordpress
+
+define('DB_NAME', 'portfoliopfuku');
+define('DB_USER', 'pfukuh01');
+define('DB_PASSWORD', 'IlbbiJitbm1519');
+define('DB_HOST', 'mysql.paigefukuhara.com');
+
+//echo DB_USER;
