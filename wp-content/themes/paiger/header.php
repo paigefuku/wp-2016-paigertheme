38b493ab4f26a720a5b6a8014a807b97f59473bb
Paige Fukuhara <paigefukuhara@gmail.com>
2016-06-07 23:29:45 -0700

    Finally got this
    I finally figured my stuff out! Fixed index.php, header.php.

--- wp-content/themes/paiger/header.php
+++ wp-content/themes/paiger/header.php
@@ -5,24 +5,19 @@
 
 <html>
 
-    <head>
-        <meta charset="utf-8">
-	
-        <title><?php bloginfo('description'); ?> | <?php bloginfo('name'); ?> | Seattle WA Photographer, Illustrator, Visual Strategist</title>
-     
-<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>" type="text/css" />
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<head>
+<meta charset="utf-8">
+<title><?php bloginfo('description'); ?> | <?php bloginfo('name'); ?> | Seattle WA Photographer, Illustrator, Visual Strategist</title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script src="<?php bloginfo('template_directory'); ?>/js/responsiveslides.js"></script>  
-<script src="<?php bloginfo('template_directory'); ?>/js/responsiveslides.min.js"></script>      
-<style>
-    
+<script src="<?php bloginfo('template_directory'); ?>/js/responsiveslides.min.js"></script>           
+<?php wp_head(); ?>
+</head>
+<body <?php body_class();?>>
 
-        </style>      
-    <?php wp_head(); ?>
-    </head>
-    <body <?php body_class();?>>
-        
-  <header><a href="home.html">   
+<header><a href="home.html">   
+<div id="header"><!--BEGIN header div, WP core-->
 <div id="logoHeader"><!--BEGIN logoHeaderdiv-->
        <img src="<?php bloginfo('template_directory'); ?>/images/papers.png" id="flyingpapers" alt="banner-top"> 
         <div id="wordsHeader"><!--BEGIN wordsHeader div-->
@@ -30,29 +25,34 @@
         <h2>Creative Services</h2>
         </div>   <!--END wordsHeader div-->
     </div><!--End logoHeaderdiv-->
+    </div><!--END header div, WP core-->
+
         </a>
-    <div id="menu"><!--BEGIN menu div-->
-        <ul id="navmenu">
-        <li>About
-            <ul class="subnavmenu">
-            <li class="subnavmenuLI"><a href="main.html">Bio</a></li>
-            <li class="subnavmenuLI"><a href="main.html">Process &amp; Methods</a></li>
-            <li class="subnavmenuLI"><a href="main.html">Friends &amp; Colleages</a></li>
-             <li class="subnavmenuLI"><a href="main.html">CV // RESUME</a></li>
+
+<div id="navigation"><!--BEGIN navigation div, WP core-->
+    <div id="navmenu"><!--BEGIN menu div-->
+        <ul>
+        <li class="nav-menu">About
+            <ul class="primary-navigation, nav-menu">
+            <li class="nav-menu"><a href="main.html" class="primary-navigation">Bio</a></li>
+            <li class="nav-menu"><a href="main.html" class="primary-navigation">Process &amp; Methods</a></li>
+            <li class="nav-menu"><a href="main.html" class="primary-navigation">Friends &amp; Colleages</a></li>
+             <li class="nav-menu"><a href="main.html" class="primary-navigation">CV // RESUME</a></li>
             </ul>
-            
-            </li>
-        <li>Services   
-         <ul class="subnavmenu">
-            <li class="subnavmenuLI"><a href="main.html">Web Services</a></li>
-            <li class="subnavmenuLI"><a href="#main.html">Illustration</a></li>
-            <li class="subnavmenuLI"><a href="main.html">Photography</a></li>
-             <li class="subnavmenuLI"><a href="main.html">Paper Crafts</a></li>
+      </li>
+     
+        <li class="nav-menu">Services   
+         <ul class="nav-menu">
+            <li class="nav-menu"><a href="main.html" class="primary-navigation">Web Services</a></li>
+            <li class="nav-menu"><a href="main.html" class="primary-navigation">Illustration</a></li>
+            <li class="nav-menu"><a href="main.html" class="primary-navigation">Photography</a></li>
+             <li class="nav-menu"><a href="main.html" class="primary-navigation">Paper Crafts</a></li>
             </ul>
-            </li> 
-        <li><a href="#">Portfolio</a></li>
-        <li><a href="#">Contact</a></li>
+         </li> 
+        <li class="nav-menu"><a href="#">Portfolio</a></li>
+        <li class="nav-menu"><a href="#">Contact</a></li>
         </ul>    
-    </div><!--END menu div-->
+    </div><!--END navigation div, WP core-->
+   </div> <!--END navmenu, WP corediv-->
     </header>
  
--- wp-content/themes/paiger/index.php
+++ wp-content/themes/paiger/index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-
+<div id="content"><!--BEGIN content div, WP core-->
 
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
@@ -15,6 +15,6 @@
 
  	<!-- REALLY stop The Loop. -->
  <?php endif; ?>
-       
+ </div><!--END content div, WP core-->     
  <?php wp_footer(); ?>
       
